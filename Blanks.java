c6d1533826a1fc01005b11c9ebfa07abdd622658
BenjaminNilssonLundqvist <benjamin.n.lundqvist@gmail.com>
2022-09-05 11:45:55 +0200

    .

---
+++ Blanks.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+// Funkar inte med inläsningen
+
+public class Blanks {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String ord=scan.next();
+        System.out.println(nbrBlanks(ord));
+    }
+    public static int nbrBlanks(String s){
+        int mellanslag=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                mellanslag++;
+            }
+        }
+        return mellanslag;
+
+    }
+
+    
+}
